c96be9e2ddc8d8a0d17dfca665a343f006d2f824
falkachu <falkachu@posteo.de>
2024-05-10 12:18:23 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+</script>
+<template>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
+</template>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const company = ref<Tables<"Company">>();
+</script>
+
+<template>
+  <div></div>
+</template>
